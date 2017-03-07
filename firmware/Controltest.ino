1ab0e9291d09733c33747e0b5f64f350028548dd
jwilli61 <jwilli61@masonlive.gmu.edu>
2017-03-07 16:39:45 -0500

    Add files via upload

---
+++ firmware/Controltest.ino
@@ -0,0 +1,34 @@
+#include <masonbot.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <TimerOne.h>
+
+
+void setup() {
+  Serial.begin(9600);
+  MasonBot();
+
+
+}
+
+void loop() {
+  
+
+
+  MasonBot().moveRotateCW();
+
+  delay(5000);
+
+  MasonBot().moveRotateCCW();
+
+  delay(5000);
+
+  MasonBot().moveForward();
+
+  delay(5000);
+
+  MasonBot().moveStop();
+
+  delay(5000);
+
+}
