d6b0cae6972b54c90f008b10d99f62817940400f
jwilli61 <jwilli61@masonlive.gmu.edu>
2017-03-16 02:43:44 -0400

    Update Rendevous.py

--- swarm1/Rendevous.py
+++ swarm1/Rendevous.py
@@ -3,12 +3,11 @@ import cmath
 import math
 from robot_structure import Robot
 
-timestamp = 0.005 # Timestamp will be removed, using for testing, will be based on robot speed.
+timestamp = .1 # Timestamp will be removed, using for testing, will be based on robot speed.
 
 def rendezvous(robot1, robot2, robot3):
     dist1 = 0  # set to zer0 or small value for rendevous
     dist2 = 0  # set to zer0 or small value for rendevous
-    print "Robot 1: x:%d y:%d dir:%d " % (robot1.xpos, robot1.ypos, robot1.dir)
 
     x1 = robot1.xpos
     x2 = robot2.xpos
@@ -33,22 +32,24 @@ def rendezvous(robot1, robot2, robot3):
     #print("moveb", moveb)
 
 
-    xx = -(movea * math.cos(phasea) + moveb * math.cos(phaseb))
-    yy = -(movea * math.sin(phasea) + moveb * math.sin(phaseb))
+    xx = (movea * math.cos(phasea) + moveb * math.cos(phaseb))
+    yy = (movea * math.sin(phasea) + moveb * math.sin(phaseb))
+    print(xx, yy)
+    finalx = robot1.xpos - xx
+    finaly = robot1.ypos - yy
+    print(finalx,finaly)
 
     (mag, phase) = cmath.polar((complex(xx,yy)))
 
     phasedeg = phase * 180 / cmath.pi - 180
-    phasedeg = int((phasedeg + 360 +180) % 360)
-
+    phasedeg = (phasedeg +360)
     rotateangle = angle - phasedeg
-    xgoal = - int(xx)
-    ygoal = -int(yy)
 
-    #print("Move dist", mag)
+
+    ##print("Move dist", mag)
     #print("angle to move to ", rotateangle)
     ### we will be moving to a set X, Y location and rotating untill a certain angle We may want to return X,Y angle.
-    return xgoal,ygoal, phasedeg
+    return finalx, finaly, phasedeg
 
 
 
