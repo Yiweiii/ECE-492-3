f049d1ea20d80ac8a690c554f79e8d5ec53a7217
mayoohee.jetaime@gmail.com <mayoohee.jetaime@gmail.com>
2017-03-27 15:03:10 -0400

    update3/27

--- swarm1/robot_structure.py
+++ swarm1/robot_structure.py
@@ -4,6 +4,11 @@ class Robot:
         self.ypos = 0
         self.dir = 0
         self.ID = ID
+		self.X_estimate = np.asmatrix(np.zeros((1,4)))
+		self.X_predict = np.asmatrix(np.zeros((1,4)))
+		self.P_estimate = np.mat([[10000,5000,0,0],[5000,5000,0,0],[0,0,10000,5000],[0,0,5000,5000]])
+		self.P_predict = np.asmatrix(np.zeros((4,4)))
+		self.count = 0
 
     def displayRobot(self):
         print
@@ -25,3 +30,10 @@ class Robot:
         self.xpos = xpos
         self.ypos = ypos
         self.dir = dir
+		
+	def setKal(count, X_estimate, X_predict, P_estimate, P_predict):
+		self.X_estimate = X_estimate 
+		self.X_predict = X_estimate 
+		self.P_estimate = X_estimate 
+		self.P_predict = P_predict
+		self.count = count
