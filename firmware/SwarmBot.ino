54bdce194462afdf0435c99748578ccf00249372
jwilli61 <jwilli61@masonlive.gmu.edu>
2017-04-16 04:20:26 -0400

    Update SwarmBot.ino
    Adding feedback controlled movement

--- firmware/SwarmBot.ino
+++ firmware/SwarmBot.ino
@@ -15,6 +15,7 @@
 #include <masonbot.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <TimerOne.h>
 
 #include <SPI.h>
@@ -31,11 +32,11 @@ int count2 =0;
 int count3 =0;
 
 int status = WL_IDLE_STATUS;
-//char ssid[] = "Verizon-SM-G930V-6155"; //  your network SSID (name) // IP address 192.168.43.95(may change)
-//char pass[] = "jasonwifi"; 
+char ssid[] = "Verizon-SM-G930V-6155"; //  your network SSID (name) // IP address 192.168.43.95(may change)
+char pass[] = "jasonwifi"; 
 
-char ssid[] = "QCHJB"; //  your network SSID (name) // IP address 192.168.43.95(may change)
-char pass[] = "robotsarecool2!"; 
+//char ssid[] = "QCHJB"; //  your network SSID (name) // IP address 192.168.43.95(may change)
+//char pass[] = "robotsarecool2!"; 
 
 int keyIndex = 1;            // your network key Index number (needed only for WEP)
 
@@ -48,6 +49,20 @@ int microseconds = 50000;
 
 //**********************Added for 3 digit velocity
 char vel[2]; //buffer for pwm input
+char Xlocch[2]; //buffer for pwm input
+char Ylocch[2]; //buffer for pwm input
+char Thetalocch[2]; //buffer for pwm input
+char Xexpectedch[2]; //buffer for pwm input
+char Yexpectedch[2]; //buffer for pwm input
+char Thetaexpectedch[2]; //buffer for pwm input
+
+float Xloc; //buffer for pwm input
+float Yloc; //buffer for pwm input
+float Thetaloc; //buffer for pwm input
+float Xexpected; //buffer for pwm input
+float Yexpected; //buffer for pwm input
+float Thetaexpected; //buffer for pwm input
+
 int velocity = 0;
 //************************************
 
@@ -61,10 +76,19 @@ WiFi.setPins(41,45,47,43); //47 CS, 45 en, 43 irq, 41 rst
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
 
+  pinMode(_ir_read_1, INPUT_PULLUP);
+  pinMode(_ir_read_2, INPUT_PULLUP);
+  pinMode(_ir_read_3, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(_ir_read_1 ), counter1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(_ir_read_2 ), counter2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(_ir_read_3 ), counter3, FALLING);
 
-//  attachInterrupt(digitalPinToInterrupt(_ir_en_1), counter1, FALLING);
-//  attachInterrupt(digitalPinToInterrupt(_ir_en_2), counter2, FALLING);
-//  attachInterrupt(digitalPinToInterrupt(_ir_en_3), counter3, FALLING);
+  pinMode(_ir_en_1, OUTPUT);
+  pinMode(_ir_en_2, OUTPUT);
+  pinMode(_ir_en_3, OUTPUT);
+  digitalWrite(_ir_en_1, HIGH);
+  digitalWrite(_ir_en_2, HIGH);
+  digitalWrite(_ir_en_3, HIGH);
 //  Timer1.initialize(microseconds);
 //  Timer1.attachInterrupt(checkspeed);
   while (!Serial) {
@@ -118,6 +142,26 @@ void loop() {
     //****************************ADDED for 3 digit velocity
     char *vel = packetBuffer + 1;
     velocity = strtoul(vel, NULL, 0);
+
+    // Controlled move,  where capital is current and lower is wanted
+    //C X.XX Y.YY ZZZ x.xx y.yy zzz
+    
+    char *Xlocch = packetBuffer + 2;
+    char *Ylocch = packetBuffer + 7;
+    char *Thetalocch = packetBuffer + 12;
+    char *Xexpectedch = packetBuffer + 16;
+    char *Yexpectedch = packetBuffer + 21;
+    char *Thetaexpectedch = packetBuffer + 26;
+
+    Xloc = atof(Xlocch);
+    Yloc = atof(Ylocch);
+    Thetaloc = atof(Thetalocch);
+    Xexpected = atof(Xexpectedch);
+    Yexpected = atof(Yexpectedch);
+    Thetaexpected = atof(Thetaexpectedch); 
+    
+
+    
     //*********************************************
     Serial.println("Contents:");
     Serial.println(packetBuffer);
@@ -128,10 +172,10 @@ void loop() {
     Udp.endPacket();
   }
   //***************************send velocity to move commands  
-  if(packetBuffer[0] == 'a'){
+  if(packetBuffer[0] == 'A'){
         MasonBot().moveRotateCW(velocity);
         packetBuffer[0] = ' ';
-  }else if(packetBuffer[0] == 'A'){
+  }else if(packetBuffer[0] == 'a'){
         MasonBot().moveRotateCCW(velocity);
          packetBuffer[0] = ' ';
   }else if(packetBuffer[0] == 'f'){
@@ -139,7 +183,13 @@ void loop() {
          packetBuffer[0] = ' ';         
   }else if(packetBuffer[0] == 's'){
         MasonBot().moveStop();
-         packetBuffer[0] = ' ';
+        packetBuffer[0] = ' ';
+  }else if(packetBuffer[0] == 'r'){
+        MasonBot().runForward(&count1);
+        packetBuffer[0] = ' ';
+  }else if (packetBuffer[0] == 'C'){
+        MasonBot().controlRun(&count1, Xloc, Yloc, Thetaloc, Xexpected, Yexpected, Thetaexpected);
+        packetBuffer[0] = ' ';
   }
 }
 
@@ -162,21 +212,23 @@ void printWiFiStatus() {
 }
 
 ////Counters for encoders
-//void counter1() {  
-//  count1 += 1;
-//  Serial.println(count1);
-//}
-//void counter2() {
-//  count2 += 1;
-//  Serial.println(count2);
-//}
-//void counter3() {
-//  count3 += 1;
-//  Serial.println(count3);
-//}
+void counter1() {  
+  count1 += 1;
+  Serial.println(count1);
+}
+
+void counter2() {
+  count2 += 1;
+//  Serial.println("C2 ");
+}
+
+void counter3() {
+  count3 += 1;
+//  Serial.println("C3 ");
+}
 
 //void checkspeed(){
-//  //Add code to counter compared to expected(based on pwm value)
+//  Add code to counter compared to expected(based on pwm value)
 //  // Compare count with wifioutpwm  ie count = 100, and wifipwm = 200 means count should be 75 lower outpwm
 //  // one revolution is 8 counts,  
 //  // if count is < expected 
@@ -191,4 +243,3 @@ void printWiFiStatus() {
 //  Serial.println(count2);
 //  Serial.println(count3);
 //}
-
