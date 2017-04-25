b7abd57da5bd187615d3f358ebcc58ee27653544
Emmanuel <etagoe@gmu.edu>
2017-04-25 04:31:35 -0400

    "Server updated"

--- swarm1/server.py
+++ swarm1/server.py
@@ -3,11 +3,20 @@ from robot_structure import Robot
 from Rendevous import rendezvous
 from direction import direction
 from messagepackage import messagepackage
+from Formation import Formation
 from camera_actions import *
 import sys
 import math
 import cv2
 import time
+
+RENDEZVOUS = 0
+FORMATION = 1
+FOLLOW = 2
+PATHFIND = 3
+PATHFOLLOW = 4
+
+ROBOT_MODE = PATHFOLLOW
 
 cap = cv2.VideoCapture(0)
 
@@ -17,10 +26,10 @@ ROBOTS = []
 video_writer = cv2.VideoWriter("output3.avi", -1, 20, (640, 480))
 
 # Create four robots
-robot1 = Robot(BLUE, BLUE, BLUE) # Blue
-robot2 = Robot(BLUE, YELLOW, YELLOW ) # Green
-robot3 = Robot(RED, RED, RED) # Red
-robot4 = Robot(YELLOW, BLUE, BLUE) # Yellow
+robot1 = Robot(YELLOW, BLUE, BLUE)
+robot2 = Robot(BLUE, YELLOW, YELLOW )
+robot3 = Robot(RED, RED, RED)
+robot4 = Robot(VIOLET, VIOLET, VIOLET)
 
 # Current map
 # Robot1 F8:F0:05:F1:D6:1C  - .6 - blue
@@ -38,41 +47,29 @@ ROBOTS.append(robot2)
 ROBOTS.append(robot3)
 ROBOTS.append(robot4)
 
-
 PORT = 2390
 BUFSIZE = 1024
 FLAG = 0
 
 udpSerSock = socket(AF_INET, SOCK_DGRAM)
 
-i = 0
-count = 1
-MESSAGE1 = "q"
-currMESSAGE1 = "q"
-MESSAGE2 = "q"
-currMESSAGE2 = "q"
-MESSAGE3 = "q"
-currMESSAGE3 = "q"
-MESSAGE4 = "q"
-currMESSAGE4 = "q"
-fwdcount1 = 1
-rotcount1 = 1
-fwdcount2 = 1
-rotcount2 = 1
-rotcount3 = 1
-fwdcount3 = 1
-rotcount4 = 1
-fwdcount4 = 1
-a1 = 1
-a2 = 1
-a3 = 1
-a4 = 1
-
 ADDR = []
+MESSAGES = []
+XPOS = []
+YPOS = []
+ANGLE = []
 for i in range(0, len(ROBOTS)):
 	address = (ROBOTS[i].HOST, PORT)
+	message = "s"
+	xpos = 0.0
+	ypos = 0.0
+	angle = 0.0
 	ADDR.append(address)
-	
+	MESSAGES.append(message)
+	XPOS.append(xpos)
+	YPOS.append(ypos)
+	ANGLE.append(angle)
+
 def update_locations(bgr_image):
 	orig_image = bgr_image.copy()
 	blur_image = cv2.medianBlur(bgr_image, 3)
@@ -103,99 +100,132 @@ def get_angle(start, next):
 	else:
 		return 0.0
 		print "error"
-		
+
+i = 0
 while True:
-    ret, bgr_image = cap.read()
-    cv2.imshow("cam_image", bgr_image)
-    height, width, channels = bgr_image.shape
-    robotwidth = 80.0
-    hsv_image = update_locations(bgr_image)
+	ret, bgr_image = cap.read()
+	cv2.imshow("cam_image", bgr_image)
+	height, width, channels = bgr_image.shape
+	robotwidth = 80.0
+	hsv_image = update_locations(bgr_image)
+
+	video_writer.write(bgr_image)
+
+	print "iteration :%d" % i
 	
-    video_writer.write(bgr_image)
+	if ROBOT_MODE == PATHFIND or ROBOT_MODE == PATHFOLLOW:
+		Map = GridWithWeights(int(round(width)/robotwidth),int(round(height)/robotwidth))
+		acquire_obstacles(hsv_image, Map, robotwidth)
 
-    print "iteration :%d" % i
-    Map = GridWithWeights(int(round(width)/robotwidth),int(round(height)/robotwidth))
-    acquire_obstacles(hsv_image, Map, robotwidth)
-    ROBOTS[3].displayRobot()
-	
-
-
-    if i == 100:
-        print "Iterations - done!"
-        MESSAGE = "stop"
-        for x in range(0, len(ROBOTS)):
-            ADDR = (ROBOTS[x].HOST, PORT)
-            udpSerSock.sendto(MESSAGE, ADDR)
-        print "Sending message : ", MESSAGE
-        cap.release()
-        video_writer.release()
-        cv2.destroyAllWindows()
-        udpSerSock.close()
-        exit(0)
-    if i > 10:
-        (xpos1, ypos1, angle1) = rendezvous(ROBOTS[0], ROBOTS[1], ROBOTS[2]) #blue
-        (xpos2, ypos2, angle2) = rendezvous(ROBOTS[1], ROBOTS[0], ROBOTS[3]) #green
-        (xpos3, ypos3, angle3) = rendezvous(ROBOTS[2], ROBOTS[3], ROBOTS[0]) #red
-        (xpos4, ypos4, angle4) = rendezvous(ROBOTS[3], ROBOTS[2], ROBOTS[1]) #yellow
+	if i == 1000:
+		print "Iterations - done!"
+		MESSAGE = "stop"
+		for x in range(0, len(ROBOTS)):
+			ADDR = (ROBOTS[x].HOST, PORT)
+			udpSerSock.sendto(MESSAGE, ADDR)
+		print "Sending message : ", MESSAGE
+		cap.release()
+		video_writer.release()
+		cv2.destroyAllWindows()
+		udpSerSock.close()
+		exit(0)
 		
-        Start = (int(round(ROBOTS[3].getX()/robotwidth)),int(round(ROBOTS[3].getY()/robotwidth)))
-        print "Robot start position:(%d,%d)" %(Start[0],Start[1])
-        Goal = (3,3)
-        if Start != Goal :
-			red_rover_path, red_rover_cost = a_star_search(Map,Start,Goal)
-			draw_grid(Map, width=3, path=reconstruct_path(red_rover_path, start=Start,goal=Goal))
-			print "\n"
-			p = Goal
-			path = []
-			for i in range(0, red_rover_cost[Goal]):
-				path.append(p)
-				p = red_rover_path[p]
+	if i > 10:
+	
+		if ROBOT_MODE == RENDEZVOUS:
+			(XPOS[0], YPOS[0], ANGLE[0]) = rendezvous(ROBOTS[0], ROBOTS[1], ROBOTS[2])
+			(XPOS[1], YPOS[1], ANGLE[1]) = rendezvous(ROBOTS[1], ROBOTS[0], ROBOTS[3])
+			(XPOS[2], YPOS[2], ANGLE[2]) = rendezvous(ROBOTS[2], ROBOTS[3], ROBOTS[0])
+			(XPOS[3], YPOS[3], ANGLE[3]) = rendezvous(ROBOTS[3], ROBOTS[2], ROBOTS[1])
+		elif ROBOT_MODE == FORMATION:
+			(XPOS[0], YPOS[0], ANGLE[0]) = Formation(ROBOTS[0], ROBOTS[1], ROBOTS[2], ROBOTS[3])
+			(XPOS[1], YPOS[1], ANGLE[1]) = Formation(ROBOTS[1], ROBOTS[0], ROBOTS[3], ROBOTS[2])
+			(XPOS[2], YPOS[2], ANGLE[2]) = Formation(ROBOTS[2], ROBOTS[3], ROBOTS[0], ROBOTS[1])
+			(XPOS[3], YPOS[3], ANGLE[3]) = Formation(ROBOTS[3], ROBOTS[2], ROBOTS[1], ROBOTS[0])
+		elif ROBOT_MODE == FOLLOW:
+			(XPOS[0], YPOS[0], ANGLE[0]) = rendezvous(ROBOTS[0], ROBOTS[1], ROBOTS[1])
+			(XPOS[1], YPOS[1], ANGLE[1]) = rendezvous(ROBOTS[1], ROBOTS[2], ROBOTS[2])
+			(XPOS[2], YPOS[2], ANGLE[2]) = rendezvous(ROBOTS[2], ROBOTS[3], ROBOTS[3])
+		elif ROBOT_MODE == PATHFIND:
+			for k in range(0, len(ROBOTS)):
+				Start = (int(round(ROBOTS[k].getX()/robotwidth)),int(round(ROBOTS[k].getY()/robotwidth)))
+				print "Robot start position:(%d,%d)" %(Start[0],Start[1])
+		
+				Goal = (0,0)
+		
+				if Start != Goal :
+					red_rover_path, red_rover_cost = a_star_search(Map,Start,Goal)
+					draw_grid(Map, width=3, path=reconstruct_path(red_rover_path, start=Start,goal=Goal))
+					print "\n"
 			
-			print(path)
-			Next = (path[red_rover_cost[Goal]-1][0],path[red_rover_cost[Goal]-1][1])
+					p = Goal
+					path = []
+					for i in range(0, red_rover_cost[Goal]):
+						path.append(p)
+						p = red_rover_path[p]
+			
+					Next = (path[red_rover_cost[Goal]-1][0],path[red_rover_cost[Goal]-1][1])
 
-			xpos4 = (Next[0]) * (robotwidth/2)
-			ypos4 = (Next[1]) * (robotwidth/2)
-			angle4 = get_angle(Start, Next)
-        else:
-			xpos4 = ROBOTS[3].xpos
-			ypos4 = ROBOTS[3].ypos
-			angle4 = ROBOTS[3].dir
-			MESSAGE4 = "s"
-			udpSerSock.sendto(MESSAGE4, ADDR[3])
-			cap.release()
-			video_writer.release()
-			cv2.destroyAllWindows()
-			udpSerSock.close()
+					XPOS[k] = (Next[0]) * (robotwidth/2)
+					YPOS[k] = (Next[1]) * (robotwidth/2)
+					ANGLE[k] = get_angle(Start, Next)
+			
+				else:
+					XPOS[k] = ROBOTS[k].xpos
+					YPOS[k] = ROBOTS[k].ypos
+					ANGLE[k] = ROBOTS[k].dir
+		elif ROBOT_MODE == PATHFOLLOW:
+			Start = (int(round(ROBOTS[3].getX()/robotwidth)),int(round(ROBOTS[3].getY()/robotwidth)))
+			print "Robot start position:(%d,%d)" %(Start[0],Start[1])
+	
+			Goal = (0,0)
+	
+			if Start != Goal :
+				red_rover_path, red_rover_cost = a_star_search(Map,Start,Goal)
+				draw_grid(Map, width=3, path=reconstruct_path(red_rover_path, start=Start,goal=Goal))
+				print "\n"
 		
-        #xpos4 = 300
-        #ypos4 = 300
-        #angle4 = 180
+				p = Goal
+				path = []
+				for i in range(0, red_rover_cost[Goal]):
+					path.append(p)
+					p = red_rover_path[p]
+			
+				Next = (path[red_rover_cost[Goal]-1][0],path[red_rover_cost[Goal]-1][1])
+
+				XPOS[3] = (Next[0]) * (robotwidth/2)
+				YPOS[3] = (Next[1]) * (robotwidth/2)
+				ANGLE[3] = get_angle(Start, Next)
 		
-        print "Robot xgoal: %d, ygoal:%d, agoal:%d" %(xpos4, ypos4, angle4)
+			else:
+				XPOS[3] = ROBOTS[3].xpos
+				YPOS[3] = ROBOTS[3].ypos
+				ANGLE[3] = ROBOTS[3].dir
+					
+			(XPOS[0], YPOS[0], ANGLE[0]) = rendezvous(ROBOTS[0], ROBOTS[1], ROBOTS[1])
+			(XPOS[1], YPOS[1], ANGLE[1]) = rendezvous(ROBOTS[1], ROBOTS[2], ROBOTS[2])
+			(XPOS[2], YPOS[2], ANGLE[2]) = rendezvous(ROBOTS[2], ROBOTS[3], ROBOTS[3])
+		else:
+			print "ERROR OCCURED"
+			exit(0)
+			
+		for k in range(0, len(ROBOTS)):
+			MESSAGES[k] = messagepackage(ROBOTS[k],XPOS[k],YPOS[k],ANGLE[k])
+			
+		for k in range(0, len(ROBOTS)):
+			if (k == 3) and ROBOT_MODE == FOLLOW:
+				break
+			else:
+				udpSerSock.sendto(MESSAGES[k], ADDR[k])
+		
+		for k in range(0, len(ROBOTS)):
+			print(k, "MESSAGE:", MESSAGES[k], "Robot xpos:", ROBOTS[k].xpos, "Robot ypos:", ROBOTS[k].ypos, "Robot dir:", ROBOTS[k].dir,
+			"Goal xpos:", XPOS[k], "Goal ypos:", YPOS[k], "Goal angle:", ANGLE[k])
 
-        MESSAGE1 = messagepackage(robot1, xpos1, ypos1, angle1)
-        MESSAGE2 = messagepackage(robot2, xpos2, ypos2, angle2)
-        MESSAGE3 = messagepackage(robot3, xpos3, ypos3, angle3)
-        MESSAGE4 = messagepackage(robot4, xpos4, ypos4, angle4)
-        udpSerSock.sendto(MESSAGE1, ADDR[0])
-        # currMESSAGE1 = MESSAGE1
-        udpSerSock.sendto(MESSAGE2, ADDR[1])
-        #   currMESSAGE2 = MESSAGE2
-        udpSerSock.sendto(MESSAGE3, ADDR[2])
-        #    currMESSAGE3 = MESSAGE3
-        udpSerSock.sendto(MESSAGE4, ADDR[3])
-        #   currMESSAGE4 = MESSAGE4
+	i = i + 1
 
-        print("robot1blue", MESSAGE1, robot1.xpos, robot1.ypos, xpos1, ypos1, robot1.dir, angle1)
-        print("robot2blueyellow", MESSAGE2, robot2.xpos, robot2.ypos, xpos2, ypos2, robot2.dir, angle2)
-        print("robot3red", MESSAGE3, robot3.xpos, robot3.ypos, xpos3, ypos3, robot3.dir, angle3)
-        print("robot4Yellow", MESSAGE4, robot4.xpos, robot4.ypos, xpos4, ypos4, robot4.dir, angle4)
-
-    i = i + 1
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
 cap.release()
 video_writer.release()
